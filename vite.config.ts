9bc7ec751b0b8dd8505d4f09bec91a2df5326efa
spencerkee <spencerkee@users.noreply.github.com>
2025-01-17 15:37:52 +0000

    Attempt to fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
 import Pages from 'vite-plugin-pages';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/age_converter/',
   plugins: [
     Pages({
       dirs: ['src/pages'],
@@ -14,5 +15,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    // TODO Maybe undo this if I feel like it.
+    minify: false
   },
 });
